a6cacd6de79cbf93b05c3182006bb4ab8fb6f8f5
AliAlrebdi <Alialrebdi@outlook.com>
2023-10-27 18:48:00 +0300

    [JS 3-exercise_1]

--- mainPageScript.js
+++ mainPageScript.js
@@ -1,16 +1,19 @@
-fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-.then(res => {
-    return res.json();
-})
-.then(data => {
-    console.log(data);
-    data.forEach(image => {
-        const markUp = `<li><img src="${image.url}"></img></li>`
-
-
-        document.querySelector("#catList").insertAdjacentHTML('afterend', markUp);
-
-    });
-
-})
-.catch(error => console.log(error));
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=1")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        data.forEach((image) => {
+          const liElement = document.createElement("li");
+          const ulCatList = document.getElementById("catList"); // No "#" symbol
+  
+          const imgElement = document.createElement("img");
+          imgElement.src = image.url;
+  
+          liElement.appendChild(imgElement);
+          ulCatList.appendChild(liElement);
+        });
+      })
+      .catch((error) => console.log(error));
+  });
+  
